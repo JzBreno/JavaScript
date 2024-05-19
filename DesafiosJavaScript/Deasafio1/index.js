32e19753c9a820c983929958169fc5dfb202808b
Jose Breno Sousa Jacauna <josebrenosousa@gmail.com>
2024-05-19 15:51:36 -0300

    up

---
+++ DesafiosJavaScript/Deasafio1/index.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById('text').innerHTML = 'Breninho gameplay'
+document.getElementById('yearsold').innerHTML = 24
+document.getElementById('work').innerHTML = 'estudo Ciencias da computação no Instituto Federal do Ceará'
+document.getElementById('anoenome').innerHTML = '2024 José Breno'
+document.getElementById('butaoinsta').innerHTML = 'Instagram'
---
+++ dia1/index.js
@@ -0,0 +1,4 @@
+const friends = ['Marcos', 'Ana']
+
+for (let i of friends)
+    console.log(i, friends[i])
